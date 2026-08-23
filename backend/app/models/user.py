0ea0d84650d4f4616d33fe5b3dc02231a9ef54fa
Theo <theomesquita@hotmail.com>
2026-08-23 11:10:13 -0300

    Definindo a proxima sprint

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -7,5 +7,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(100), nullable=False)
     email = Column(String(150), unique=True, index=True, nullable=False)
-    senha_hash = Column(String(255), nullable=False)
     ativo = Column(Boolean, default=True)
